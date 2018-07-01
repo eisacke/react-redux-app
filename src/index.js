0d36f4413113b4a14ba2e06e2fc22b5785cd946b
eisacke <emily@isacke.com>
2018-07-01 22:50:54 +0100

    Case sensitive change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 const store = configureStore();

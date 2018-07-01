d7f6ddbea83ee2762cb45f497ad1a3ba3b8dfd45
eisacke <emily@isacke.com>
2018-07-01 23:13:12 +0100

    Heroku deployment

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.static(`${__dirname}/public`));
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => console.log(`Express is listening on port ${port}`));
